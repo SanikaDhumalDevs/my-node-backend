6c9df1da51a56c048781944008ff64db913e562c
Sanika Dhumal <sanikadhumal149@gmail.com>
2026-05-28 19:58:06 +0530

    H

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.use((err, req, res, next) => {
 
 // ✅ Connect to MongoDB and start server
 // ✅ Connect to MongoDB and start server
-mongoose.connect("mongodb+srv://expense_user:SecurePass123@cluster0.yourcluster.mongodb.net/expense_tracker?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://expense_user:SecurePass123@cluster0.ikbfivb.mongodb.net/expense_tracker?appName=Cluster0", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
